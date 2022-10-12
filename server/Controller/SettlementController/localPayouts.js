40ec89b94f8c1e814d146237c1984f34306fac35
Shivanshu Upadhyay <shivanshu6525@gmail.com>
2022-10-12 11:47:43 +0530

    new

--- server/Controller/SettlementController/localPayouts.js
+++ server/Controller/SettlementController/localPayouts.js
@@ -14,10 +14,10 @@ class LocalPayouts{
             let limit = req.body.limit ? Number(req.body.limit) : 10;
             let { start, numOfPages } = Pagination.pagination(result[0].Total, page, limit);
             
-            let sql='select * from tbl_icici_payout_transaction_response_details ORDER BY created_on DESC limit ?,?'
-            let sqlDate = 'select * from tbl_icici_payout_transaction_response_details where DATE(created_on) = ? ORDER BY created_on DESC limit ?,?'
-            let sqlToFrom = 'select * from tbl_icici_payout_transaction_response_details where DATE(created_on)  >= ? AND DATE(created_on) <= ? ORDER BY created_on DESC limit ?,?'
-            let sqlSearch = `select * from tbl_icici_payout_transaction_response_details where creditacc LIKE '%${searchItem}%' ORDER BY created_on DESC limit ?,?`
+            let sql='select payout_bank,trx_from,uniqueid,customer_order_id,status,response,utrnumber,trx_type,payee_name,creditacc,ifsc,amount,remark,gst_amount,bank_charges,wallet_deduct,currency,created_on,updated_on from tbl_icici_payout_transaction_response_details ORDER BY created_on DESC limit ?,?'
+            let sqlDate = 'select payout_bank,trx_from,uniqueid,customer_order_id,status,response,utrnumber,trx_type,payee_name,creditacc,ifsc,amount,remark,gst_amount,bank_charges,wallet_deduct,currency,created_on,updated_on from tbl_icici_payout_transaction_response_details where DATE(created_on) = ? ORDER BY created_on DESC limit ?,?'
+            let sqlToFrom = 'select payout_bank,trx_from,uniqueid,customer_order_id,status,response,utrnumber,trx_type,payee_name,creditacc,ifsc,amount,remark,gst_amount,bank_charges,wallet_deduct,currency,created_on,updated_on from tbl_icici_payout_transaction_response_details where DATE(created_on)  >= ? AND DATE(created_on) <= ? ORDER BY created_on DESC limit ?,?'
+            let sqlSearch = `select payout_bank,trx_from,uniqueid,customer_order_id,status,response,utrnumber,trx_type,payee_name,creditacc,ifsc,amount,remark,gst_amount,bank_charges,wallet_deduct,currency,created_on,updated_on from tbl_icici_payout_transaction_response_details where creditacc LIKE '%${searchItem}%' ORDER BY created_on DESC limit ?,?`
 
             const data = await mysqlcon(date?sqlDate:(to&&from)?sqlToFrom:searchItem?sqlSearch:sql,date?[date,start,limit]:(to&&from)?[from,to,start,limit]:[start,limit]);
             res.status(200).json({
