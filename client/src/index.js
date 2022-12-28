ffa0fe44477bd0aec94bc88cf614ec72ae965738
shivanshu <shivanshu6525@gmail.com>
2022-12-28 16:12:33 +0530

    new

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import ContextProvider from "./context/ContextProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <ContextProvider>
+     <ContextProvider> 
       <App />
     </ContextProvider>
   </React.StrictMode>
